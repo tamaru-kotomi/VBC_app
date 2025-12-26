e1f87202352d802e7614d3361cd982c918b80e8e
tamaru kotomi <kotomi.tamaru@ysinc.co.jp>
2025-12-26 18:12:00 +0900

    chore: next.config.ts の追記

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  // 1. Reactの厳格モードを有効化
+  reactStrictMode: true,
+
+  // 2. 本番環境でのconsole.logを消す
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+
+  // 3. 画像のドメイン許可
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
